b43a185b6cb47114eff8e9c9be8da78d8e84e74d
raajaws <raajaws@gmail.com>
2023-04-27 16:01:05 -0400

    test from git

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World Rajesh',
+        'output': 'Hello World Rajesh Singh',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
