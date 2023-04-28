970a302b56f2465e999c3af2ebe0936f3ee250c5
singhr6state <131484539+singhr6state@users.noreply.github.com>
2023-04-28 13:10:49 -0400

    Update index.py
    done

--- index.py
+++ index.py
@@ -4,7 +4,7 @@ import datetime
 
 def handler(event, context):
     data = {
-        'output': 'Hello World Rajesh Singh',
+        'output': 'Hello World Rajesh Singh - 04/28/2023 1:10 PM',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
